0f0306b5967f0768251364af377d8bde4fe48fe8
evrinoma <evrinoma@gmail.com>
2021-03-09 02:12:07 +0300

    fix path in vue components

--- src/Resources/public/js/AgGrid/ContrAgent/AgContrAgent.vue
+++ src/Resources/public/js/AgGrid/ContrAgent/AgContrAgent.vue
@@ -1,145 +1,145 @@
 <template>
-    <div>
-        <div class="ui segment block">
-            <h3 class="ui header">ContrAgent viewer</h3>
-            <div class="ui segment segment_top">
-                <div class="ui two column very relaxed grid">
-                    <!--                    <div style="background-color:#ccc; padding: 2rem">left side column</div>-->
-                    <div class="column">
-                        <!--                        ag-grid-angular style="height: 100%; width:100%" *-->
-                        <ag-grid-vue style="width: 100%; height: 100%;"
-                                     class="ag-theme-balham-dark"
-                                     :columnDefs="columnDefs"
-                                     :rowData="rowData"
-                                     :editType="editType"
-                                     rowSelection="multiple"
-                                     @grid-ready="onGridReady"
-                                     :components="components"
-                                     @first-data-rendered="onFirstDataRendered"
-                        >
-                        </ag-grid-vue>
-                    </div>
-                    <!--                    &lt;!&ndash;                    <div style="background-color:#ccc; padding: 2rem">right side column</div>&ndash;&gt;-->
-                    <!--                    <div class="column">-->
-                    <!--                    </div>-->
-                </div>
-            </div>
-            <h4 class="ui horizontal divider custom_divider">
-                <i class="resize vertical icon"></i>
-            </h4>
-            <div class="ui segment segment_bottom">
-                <div style="background-color:#ccc; margin-top: -10px;">right side column</div>
-            </div>
+  <div>
+    <div class="ui segment block">
+      <h3 class="ui header">ContrAgent viewer</h3>
+      <div class="ui segment segment_top">
+        <div class="ui two column very relaxed grid">
+          <!--                    <div style="background-color:#ccc; padding: 2rem">left side column</div>-->
+          <div class="column">
+            <!--                        ag-grid-angular style="height: 100%; width:100%" *-->
+            <ag-grid-vue style="width: 100%; height: 100%;"
+                         class="ag-theme-balham-dark"
+                         :columnDefs="columnDefs"
+                         :rowData="rowData"
+                         :editType="editType"
+                         rowSelection="multiple"
+                         @grid-ready="onGridReady"
+                         :components="components"
+                         @first-data-rendered="onFirstDataRendered"
+            >
+            </ag-grid-vue>
+          </div>
+          <!--                    &lt;!&ndash;                    <div style="background-color:#ccc; padding: 2rem">right side column</div>&ndash;&gt;-->
+          <!--                    <div class="column">-->
+          <!--                    </div>-->
         </div>
+      </div>
+      <h4 class="ui horizontal divider custom_divider">
+        <i class="resize vertical icon"></i>
+      </h4>
+      <div class="ui segment segment_bottom">
+        <div style="background-color:#ccc; margin-top: -10px;">right side column</div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import {AgGridVue} from "ag-grid-vue";
-    import {fetch} from 'whatwg-fetch';
-    import DatePicker from '../../components/AgGrid/DatePicker';
+import {AgGridVue} from "ag-grid-vue";
+import {fetch} from 'whatwg-fetch';
 
-    export default {
-        name: 'agContrAgentVue',
-        data() {
-            return {
-                gridOptions: null,
-                gridApi: null,
-                columnApi: null,
-                columnDefs: null,
-                defaultColDef: null,
-                components: null,
-                editType: null,
-                rowData: null,
-            }
-        },
-        components: {
-            AgGridVue
-        },
-        methods: {
-            //автоматический ресайз всех столбцов в ширину окна таблицы
-            onFirstDataRendered(params) {
-                params.api.sizeColumnsToFit();
-            },
-            loadRowData() {
-                fetch(location.protocol + '//' + location.hostname + '/evrinoma/api/contr_agent')
-                    .then(result => result.json())
-                    .then(rowData => this.rowData = rowData);
-            },
-            onGridReady(params) {
-                this.gridApi = params.api;
-                this.columnApi = params.columnApi;
-            },
-        },
-        beforeMount() {
-            fetch(location.protocol + '//' + location.hostname + '/evrinoma/api/contr_agent/column_defs')
-                .then(result => result.json())
-                .then(rowData => this.columnDefs = rowData);
-            //помпонеты редактирования
-            this.components = {datePicker: getDatePicker()};
-            //полнострочное редактирование
-            this.editType = 'fullRow';
-        },
-        created() {
-            this.loadRowData();
-        }
+export default {
+  name: 'agContrAgentVue',
+  data() {
+    return {
+      gridOptions: null,
+      gridApi: null,
+      columnApi: null,
+      columnDefs: null,
+      defaultColDef: null,
+      components: null,
+      editType: null,
+      rowData: null,
     }
+  },
+  components: {
+    AgGridVue
+  },
+  methods: {
+    //автоматический ресайз всех столбцов в ширину окна таблицы
+    onFirstDataRendered(params) {
+      params.api.sizeColumnsToFit();
+    },
+    loadRowData() {
+      fetch(location.protocol + '//' + location.hostname + '/evrinoma/api/contr_agent')
+          .then(result => result.json())
+          .then(rowData => this.rowData = rowData);
+    },
+    onGridReady(params) {
+      this.gridApi = params.api;
+      this.columnApi = params.columnApi;
+    },
+  },
+  beforeMount() {
+    fetch(location.protocol + '//' + location.hostname + '/evrinoma/api/contr_agent/column_defs')
+        .then(result => result.json())
+        .then(rowData => this.columnDefs = rowData);
+    //помпонеты редактирования
+    this.components = {datePicker: getDatePicker()};
+    //полнострочное редактирование
+    this.editType = 'fullRow';
+  },
+  created() {
+    this.loadRowData();
+  }
+}
 </script>
 
 <style lang="scss">
-    @import "../../../../../../../node_modules/ag-grid-community/dist/styles/ag-grid.css";
-    @import "../../../../../../../node_modules/ag-grid-community/dist/styles/ag-theme-balham-dark.css";
 
-    .ui.segment.block {
-        height: 90vh;
-    }
+@import "~ag-grid-community/dist/styles/ag-grid.css";
+@import "~ag-grid-community/dist/styles/ag-theme-balham-dark.css";
 
-    div#footer {
-        height: 35px;
-    }
+.ui.segment.block {
+  height: 90vh;
+}
 
-    div .ag-grid {
-        display: block;
-        width: 100%;
-    }
+div#footer {
+  height: 35px;
+}
 
-    /* верхний блок */
-    div.ui.segment_top {
-        width: 100%;
-        height: 85%;
-    }
+div .ag-grid {
+  display: block;
+  width: 100%;
+}
 
-    /* нижний блок */
-    div.ui.segment_bottom {
-        width: 100%;
-        height: 10%;
-    }
+/* верхний блок */
+div.ui.segment_top {
+  width: 100%;
+  height: 85%;
+}
 
-    .ui[class*="two column"].grid > .column:not(.row), .ui[class*="two column"].grid > .row > .column {
-        width: 100%;
-    }
+/* нижний блок */
+div.ui.segment_bottom {
+  width: 100%;
+  height: 10%;
+}
 
-    div.ui.two.column.very.relaxed.grid {
-        height: 105%;
-        width: 100%;
-    }
+.ui[class*="two column"].grid > .column:not(.row), .ui[class*="two column"].grid > .row > .column {
+  width: 100%;
+}
 
-    .ui.divider.custom_divider {
-        margin: -1rem 0;
-    }
+div.ui.two.column.very.relaxed.grid {
+  height: 105%;
+  width: 100%;
+}
 
-    /*подстветка столюца и строки*/
-    .ag-row-hover {
-        /* putting in !important so it overrides the theme's styling as it hovers the row also */
-        background-color: #525255 !important;
-    }
+.ui.divider.custom_divider {
+  margin: -1rem 0;
+}
 
-    .ag-column-hover {
-        background-color: #2f402f;
-    }
+/*подстветка столюца и строки*/
+.ag-row-hover {
+  /* putting in !important so it overrides the theme's styling as it hovers the row also */
+  background-color: #525255 !important;
+}
 
-    /*DatePicker*/
-    .hasDatepicker {
-        background-color: #2d3436;
-    }
+.ag-column-hover {
+  background-color: #2f402f;
+}
+
+/*DatePicker*/
+.hasDatepicker {
+  background-color: #2d3436;
+}
 </style>
